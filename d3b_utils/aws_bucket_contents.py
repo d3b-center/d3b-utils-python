0abd099911356b27faebe4e5cf4b5707c6ae0099
Chris Friedman <chris.s.friedman@gmail.com>
2021-07-21 18:15:48 -0400

    :sparkles: add ability to scrape object versions
    :rewind: don't delete backwards compatability
    
    :recycle: dont return contents if file_version=true
    
    only return file versions and delete markers
    
    :memo: add docstrings for get_versions
    
    also modifies some docstrings as appropriate

--- d3b_utils/aws_bucket_contents.py
+++ d3b_utils/aws_bucket_contents.py
@@ -1,7 +1,8 @@
-import boto3
-import re
 import csv
+import re
 from itertools import chain
+
+import boto3
 
 
 def fetch_bucket_obj_info(
@@ -11,6 +12,7 @@ def fetch_bucket_obj_info(
     output_filename=None,
     delim=None,
     profile="saml",
+    get_versions=False,
 ):
     """
     List the objects of a bucket and the size of each object. Returns a
@@ -28,16 +30,31 @@ def fetch_bucket_obj_info(
         This is done by removing objects where `Size = 0`. Default is
         False.
     :type drop_folders: bool, optional
-    :param output_filename: If provided, write delimited results to this file
+    :param output_filename: If provided, write delimited results to this
+        file. If get_versions is False, writes a single file. If
+        get_versions is True, writes two files:
+        "versions-{output_filename}" for the file versions and
+        "delete_markers-{output_filename}" for the delete markers
     :type output_filename: str, optional
     :param delim: If writing output to a delimited file, use this delimiter
     :type delim: str, optional, default based on output_filename extension
     :param profile: aws profile
     :type profile: str, optional
+    :param get_versions: get object versions (and delete markers) instead of
+        only getting current bucket contents.
+    :type get_versions: bool, optional
 
-    :returns: list of dicts, where each dict has information about each
-        object in the bucket (or each object that has a path matching
-        the search_prefixes).
+    :returns: list of dicts. If get_versions is False, each dict has
+        information about each object in the bucket (or each object that has
+        a path matching the search_prefixes). If get_versions is True, the
+        list of dicts has two items, versions and delete_markers, each
+        containing a list of dicts of versions and delete_markers
+        respectively, i.e.:
+        {
+            versions: [{version-a}, ... {version-n}],
+            delete_markers: [{delete_marker-a}, ... {delete_marker-n}],
+        }
+
     """
     search_prefixes = search_prefixes or ""
 
@@ -52,36 +69,80 @@ def fetch_bucket_obj_info(
 
     paginator = client.get_paginator("list_objects_v2")
 
-    # Build the bucket contents
-    bucket_contents = []
-    for key_prefix in search_prefixes:
-        for page in paginator.paginate(Bucket=bucket_name, Prefix=key_prefix):
-            bucket_contents.extend(page.get("Contents", []))
-
-    if drop_folders:
-        bucket_contents = [
-            object for object in bucket_contents 
-            if ((object["Key"][-1] != "/") or (object["Size"] > 0))
-        ]
-
-    for object in bucket_contents:
-        object["Bucket"] = bucket_name
-        # ETag comes back with unnecessary quotation marks, so strip them
-        object["ETag"] = object["ETag"].strip('"')
-
-    # Write to file
+    # Prep file writing
     if output_filename:
         delimiters = {"tsv": "\t", "csv": ","}
         delim = delim or delimiters[output_filename.rsplit(".", 1)[-1].lower()]
         assert delim, "File output delimiter not known. Cannot proceed."
-        keys = set(chain(*(d.keys() for d in bucket_contents)))
-        with open(output_filename, "w") as f:
-            dict_writer = csv.DictWriter(
-                f, restval="", fieldnames=keys, delimiter=delim
-            )
-            dict_writer.writeheader()
-            dict_writer.writerows(bucket_contents)
 
+    if get_versions:
+        version_paginator = client.get_paginator("list_object_versions")
+        bucket_contents = {"versions": [], "delete_markers": []}
+        for key_prefix in search_prefixes:
+            for page in version_paginator.paginate(
+                Bucket=bucket_name, Prefix=key_prefix
+            ):
+                bucket_contents["versions"].extend(page.get("Versions", []))
+                bucket_contents["delete_markers"].extend(
+                    page.get("DeleteMarkers", [])
+                )
+        if drop_folders:
+            bucket_contents["versions"] = [
+                version
+                for version in bucket_contents["versions"]
+                if ((version["Key"][-1] != "/") or (version["Size"] > 0))
+            ]
+            bucket_contents["delete_markers"] = [
+                marker
+                for marker in bucket_contents["delete_markers"]
+                if ((marker["Key"][-1] != "/"))
+            ]
+        for version in bucket_contents["versions"]:
+            version["Bucket"] = bucket_name
+            # ETag comes back with unnecessary quotation marks, so strip them
+            version["ETag"] = version["ETag"].strip('"')
+        for marker in bucket_contents["delete_markers"]:
+            marker["Bucket"] = bucket_name
+        # Write to file
+        if output_filename:
+            for part in bucket_contents.keys():
+                keys = set(chain(*(d.keys() for d in bucket_contents[part])))
+                with open(part + "-" + output_filename, "w") as f:
+                    dict_writer = csv.DictWriter(
+                        f, restval="", fieldnames=keys, delimiter=delim
+                    )
+                    dict_writer.writeheader()
+                    dict_writer.writerows(bucket_contents[part])
+    else:
+        paginator = client.get_paginator("list_objects_v2")
+        # Build the bucket contents
+        bucket_contents = []
+        for key_prefix in search_prefixes:
+            for page in paginator.paginate(
+                Bucket=bucket_name, Prefix=key_prefix
+            ):
+                bucket_contents.extend(page.get("Contents", []))
+        if drop_folders:
+            bucket_contents = [
+                object
+                for object in bucket_contents
+                if ((object["Key"][-1] != "/") or (object["Size"] > 0))
+            ]
+
+        for object in bucket_contents:
+            object["Bucket"] = bucket_name
+            # ETag comes back with unnecessary quotation marks, so strip them
+            object["ETag"] = object["ETag"].strip('"')
+
+        # Write to file
+        if output_filename:
+            keys = set(chain(*(d.keys() for d in bucket_contents)))
+            with open(output_filename, "w") as f:
+                dict_writer = csv.DictWriter(
+                    f, restval="", fieldnames=keys, delimiter=delim
+                )
+                dict_writer.writeheader()
+                dict_writer.writerows(bucket_contents)
     return bucket_contents
 
 
